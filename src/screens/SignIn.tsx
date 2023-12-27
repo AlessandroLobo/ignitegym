4889489db22fc79991e7a39cd2d99273facbfcea
Alessandro Lobo <alessandro.lobo@hotmail.com>
2023-12-27 10:30:52 -0300

    add Botton Tab Navigator

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -5,15 +5,26 @@ import BackgroundImg from "@assets/background.png";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import { useNavigation } from "@react-navigation/native";
 
 export function SignIn() {
+
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  function handleNewAccount() {
+    navigation.navigate("SignUp");
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
-      <VStack flex={1} bg="gray.700" px="10" pb={16}>
-        <Image source={BackgroundImg}
+      <VStack flex={1} px="10" pb={16}>
+        <Image
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
           alt="Pessoas treinando"
           resizeMode="contain"
           position="absolute"
@@ -50,7 +61,11 @@ export function SignIn() {
           >
             Ainda não tem acesso?
           </Text>
-          <Button title="Criar Conta" variant="outline" />
+          <Button
+            title="Criar Conta"
+            variant="outline"
+            onPress={handleNewAccount}
+          />
         </Center>
 
       </VStack>
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 
 import LogoSvg from "@assets/logo.svg";
@@ -7,13 +8,21 @@ import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export function SignUp() {
+
+  const navigation = useNavigation();
+
+  function handleNewGoBack() {
+    navigation.goBack();
+  }
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
-      <VStack flex={1} bg="gray.700" px="10" pb={16}>
-        <Image source={BackgroundImg}
+      <VStack flex={1} px="10" pb={16}>
+        <Image
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
           alt="Pessoas treinando"
           resizeMode="contain"
           position="absolute"
@@ -47,6 +56,7 @@ export function SignUp() {
             title="Voltar para o Login"
             variant="outline"
             mt={24}
+            onPress={handleNewGoBack}
           />
         </Center>
 
