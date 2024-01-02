af01f38ec7e72be671f59cc875c3bed778d72508
Alessandro Lobo <alessandro.lobo@hotmail.com>
2024-01-02 10:41:34 -0300

    add loadin photo perfil and filesystem

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,15 +1,65 @@
-import { Center, ScrollView, VStack, Skeleton, Text, Heading } from "native-base";
+
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from "native-base";
 import { ScreenHeader } from "@components/ScreeeHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
+  const [userPhoto, setUserPhoto] = useState('https://github.com/AlessandroLobo.png')
+
+  const toast = useToast()
+
+  // Carrega a foto da galeria
+  async function handleUserPhotoSelect() {
+    setPhotoIsLoading(true)
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
+
+      if (photoSelected.canceled) {
+        alert('Selecione uma foto')
+        return
+      }
+
+      if (photoSelected.assets) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)
+
+        if (photoInfo.exists && photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+          return toast.show({
+
+            //usando toast --------------------------------------------------------------
+            title: 'Erro ao carrregar a imagem',
+            description: 'Imagem muito grande Escolha uma imagem menor que 5mb',
+            placement: 'top',
+            bgColor: 'red.500',
+          })
+          //usando toast ----------------------------------------------------------------
+
+        } else {
+          setUserPhoto(photoSelected.assets[0].uri)
+        }
+
+      }
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setPhotoIsLoading(false)
+    }
+  }
+
 
   return (
     <VStack flex={1}>
@@ -26,13 +76,13 @@ export function Profile() {
             />
             :
             <UserPhoto
-              source={{ uri: 'https://github.com/AlessandroLobo.png' }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
           }
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
               Alterar foto
             </Text>
